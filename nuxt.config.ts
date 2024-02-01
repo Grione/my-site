33ec2775bbb29f3840dbf1637ccf60edc682dc18
Grigoriy Gavrilov <greeb.spb@gmail.com>
2024-02-01 19:58:32 +0600

    add base dato cms query

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    datoCmsToken: process.env.DATO_CMS_TOKEN,
+  },
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{data.page.title}}</h1>
+    <p>Data: <code>{{ data }}</code></p>
+  </div>
+</template>
+
+<script setup>
+const QUERY = `
+  query {
+    page(filter:{id:{eq: "QS1LLvUZSr2gG0uZULfcIQ"}}) {
+      id
+      title
+      _status
+    }
+  }
+`;
+const { data, error } = await useGraphqlQuery({ query: QUERY });
+</script>
